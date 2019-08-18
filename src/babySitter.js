62721f0492dfd8e0fbc3ed8b3d1f6745bcb23510
Kathy <kpnelson847@gmail.com>
2019-08-18 18:35:43 -0400

    6th test passes

--- src/babySitter.js
+++ src/babySitter.js
@@ -1,7 +1,12 @@
 "use strict";
 function startJob(startTime) {
+    //StartTime between 5pm and midnight
     if (startTime >= 17 && startTime <=24) {
         return startTime;
+    //StartTime between 1am - 3am (cannot be 4am since that is the latest end time)
+    } else if (startTime >= 1 && startTime <=3) {
+        return startTime + 24;
+    } else {
+    return 17;
     }
-    return startTime;
 };
--- tests/babySitter.spec.js
+++ tests/babySitter.spec.js
@@ -11,10 +11,21 @@ describe("Tests for BabySitter Kata", function() {
         expect(startJob(20)).toBe(20);
     });
 
-    it("Testing startJob to accept the startTime that is in range", function() {
+    it("Testing startJob to accept the startTime that is in range (before midnight)", function() {
         expect(startJob(17)).toBe(17);
         expect(startJob(20)).toBe(20);
+    });
+    //startTime can start no earlier than 5pm (17:00)
+    it("Testing to see if startJob will take a time that is not in range and return a time in range", function() {
+        expect(startJob(16)).toBe(17);
+        expect(startJob(5)).toBe(17);
     })
+
+    //To have the code not break after midnight, I chose to add +24 to the numbers that start after midnight
+    it("Testing to see startJob will take a time in range (after midnight) and return an appropriate number", function() {
+        expect(startJob(3)).toBe(27);
+        expect(startJob(2)).toBe(26);
+    });
 
 
 }); //end test case
